5cde0491645bf16ad29c68c5bd10de06d2d66a21
jiveshgoel010 <jiveshgoel010@gmail.com>
2024-04-06 20:06:11 +0530

    Reconfigured the login and signup routes

--- backend/server/routes/main.js
+++ backend/server/routes/main.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+// require('../views/layouts/index');
+const indexLayout = '../views/layouts/index-layout';
+
 //Routes
 //GET - ROOT
 router.get('/', (req, res) => {
     try {
-        res.render('home');
+        res.render('index', {layout: indexLayout});
     } catch (error) {
         console.log(error);
     }
 });
+
+//GET - home
+router.get('/home',(req,res)=>{
+    try{
+        res.render('home');
+    } catch(error){
+        console.log(error);
+    }
+})
 
 
 //GET - BMI
